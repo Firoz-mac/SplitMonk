929296cfd17fe26889f45264ab98d25b6de652c1
Firoz <firozmhd23@gmail.com>
2026-04-03 17:10:54 +0530

    updated cores handling

--- server/server.js
+++ server/server.js
@@ -21,12 +21,23 @@ await connectDB();
 await connectCloudinary()
 
 //allow origins
-const allowedOrigins=['http://localhost:5173']
+const allowedOrigins=['http://localhost:5173', 'http://127.0.0.1:5173']
 
 //middleware config
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, credentials:true}));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed"));
+    }
+  },
+  credentials: true
+}));
 
 
 const server = http.createServer(app);
